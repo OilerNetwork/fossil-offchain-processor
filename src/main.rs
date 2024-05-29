dd929deab0483861d155fbd6e05260ebb23f6c1c
Sourabh Jaiswal <sourabhrj31@gmail.com>
2024-05-30 00:51:08 +0800

    connecting with blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 mod controller;
 mod model;
+use std::str::FromStr;
+
 use axum::{
     extract::{FromRef, MatchedPath},
     http::Request,
     routing::post,
     Router,
 };
+use fossil_offchain_processor::relayer::contract::TestContract;
 use reqwest::Client;
 
 use dotenv::dotenv;
+use secp256k1::SecretKey;
 use tower_http::trace::TraceLayer;
 use tracing::info_span;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -66,6 +70,17 @@ async fn main() {
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    let transport = web3::transports::Http::new("http://127.0.0.1:8545").unwrap();
+    let web3 = web3::Web3::new(transport);
+
+    let test_contract = TestContract::new(
+        &web3,
+        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".to_string(),
+    )
+    .await;
+
+    test_contract.send_latest_parent_has_to_l2().await;
 
     tracing::info!("listening on http://{}", listener.local_addr().unwrap());
 
