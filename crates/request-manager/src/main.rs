d501f5d1c4f684ce23463f0e7b3eac8c5b650c65
c0np4nn4 <c0np4nn4@gmail.com>
2024-06-12 13:06:02 +0900

    refactor: organize main.rs and modularize handlers and state

--- crates/request-manager/src/main.rs
+++ crates/request-manager/src/main.rs
@@ -1,45 +1,23 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
 use axum::{
     extract::{FromRef, MatchedPath},
-    http::Request,
+    http::{Request, StatusCode},
     routing::post,
     Router,
 };
-use reqwest::Client;
-
 use dotenv::dotenv;
-use serde::Deserialize;
+use reqwest::Client;
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::Mutex;
 use tower_http::trace::TraceLayer;
 use tracing::info_span;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use proof_generator::controller::mev_blocker::call_mev_blocker_api;
-use utils::get_storage_value;
+mod handlers;
+mod state;
+mod utils;
 
-pub mod utils;
-
-#[derive(Clone)]
-struct AppState {
-    client: Client,
-    storage_cache: Arc<Mutex<HashMap<String, String>>>,
-}
-
-// support converting an `AppState` in an `ApiState`
-impl FromRef<AppState> for Client {
-    fn from_ref(app_state: &AppState) -> Client {
-        app_state.client.clone()
-    }
-}
-
-#[derive(Deserialize)]
-struct StorageRequest {
-    account_address: String,
-    storage_key: String,
-}
+use handlers::get_storage_value::get_storage_value;
+use state::AppState;
 
 #[tokio::main]
 async fn main() {
@@ -59,13 +37,9 @@ async fn main() {
     };
 
     let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", post(call_mev_blocker_api))
         .route("/getStorageValue", post(get_storage_value))
         .layer(
             TraceLayer::new_for_http().make_span_with(|request: &Request<_>| {
-                // Log the matched route's path (with placeholders not filled in).
-                // Use request.uri() or OriginalUri if you want the real path.
                 let matched_path = request
                     .extensions()
                     .get::<MatchedPath>()
@@ -81,10 +55,7 @@ async fn main() {
         )
         .with_state(app_state);
 
-    // run our app with hyper, listening globally on port 8000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-
     tracing::info!("listening on http://{}", listener.local_addr().unwrap());
-
     axum::serve(listener, app).await.unwrap();
 }
--- crates/request-manager/src/utils.rs
+++ crates/request-manager/src/utils.rs
@@ -1,31 +1,7 @@
-use axum::{extract::State, response::IntoResponse, Json};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
 
-use crate::{AppState, StorageRequest};
-
-pub async fn get_storage_value(
-    State(app_state): State<AppState>, //
-    Json(input): Json<StorageRequest>,
-) -> impl IntoResponse {
-    let account_address = input.account_address;
-    let storage_key = input.storage_key;
-
-    let cache_key = format!("{}:{}", account_address, storage_key);
-
-    let mut cache = app_state.storage_cache.lock().unwrap();
-
-    if let Some(cached_value) = cache.get(&cache_key) {
-        return Json(serde_json::json!({
-            "status": "cached",
-            "value": cached_value,
-        }))
-        .into_response();
-    }
-
-    drop(cache); // unlock the mutex
-
-    Json(serde_json::json!({
-        "status": "not_found_in_cache",
-        "message": "Proceed to fetch from Ethereum Data Dispatcher"
-    }))
-    .into_response()
+pub fn response_with_status(status: StatusCode, message: &str) -> Response {
+    (status, Json(serde_json::json!({ "message": message }))).into_response()
 }
