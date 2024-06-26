ffda4e6b8f9e0858b82a714562abd94f6662b105
c0np4nn4 <c0np4nn4@gmail.com>
2024-06-26 13:23:17 +0900

    wip: leave the TODO list
    based on the sequence diagram

--- crates/request-manager/src/handlers/get_storage_value.rs
+++ crates/request-manager/src/handlers/get_storage_value.rs
@@ -85,7 +85,7 @@ pub async fn get_storage_value(
             tracing::error!("Error response_storage: {:?}", err);
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                Json("Error at respond_storage"),
+                Json("Error, respond_storage"),
             )
                 .into_response();
         }
@@ -105,6 +105,45 @@ pub async fn get_storage_value(
         // 2. request storage proof by calling `call_mev_blocker_api` in proof-generator
         let api_input = Input {
             account_address: input.account_proof.address.to_string(),
+            storage_keys: input.storage_keys.clone(),
+        };
+
+        // I think the name `call_mev_blocker_api` should be changed
+        let response = call_mev_blocker_api(State(app_state.client.clone()), Json(api_input))
+            .await
+            .into_response();
+
+        // TODO let storage_proof = func(response) ...
+
+        // 3. request state_root by calling `get_state_root` in l1_headers_store
+        let state_root = l1_headers_store_contract
+            .get_state_root(
+                input.block_number, //
+            )
+            .await;
+
+        let state_root = match state_root {
+            Ok(res) => {
+                tracing::info!("Result state_root: {:?}", res.len());
+                res
+            }
+            Err(err) => {
+                tracing::error!("Error state_root: {:?}", err);
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json("Error, get_state_root"),
+                )
+                    .into_response();
+            }
+        };
+
+        // TODO 3-ALT.
+        //        request the RLP Encoded Block from the proof_generator
+        //        then process the block with the starknet_handler, which will save the state_root
+
+        // TODO 4. request storage proof again
+        let api_input = Input {
+            account_address: input.account_proof.address.to_string(),
             storage_keys: input.storage_keys,
         };
         let response = call_mev_blocker_api(State(app_state.client), Json(api_input))
@@ -113,8 +152,13 @@ pub async fn get_storage_value(
 
         // TODO let storage_proof = func(response) ...
 
+        // TODO 5. check if Account Already verified
+        // TODO 5-ALT. Account not verified, verify Account proof
+        // TODO 5-ALT2. error RESPONSE, Proof INVALID
+
+        // TODO 6. verify storage proof
+        // TODO 6-ALT. proof invalid, error RESPONSE
+
         return (StatusCode::OK, Json("wip")).into_response();
     }
-
-    // Ok(Response::default())
 }
