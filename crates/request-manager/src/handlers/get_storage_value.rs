6932898f10a10de4f33119cafe1f121df91b0c00
c0np4nn4 <c0np4nn4@gmail.com>
2024-06-26 13:59:40 +0900

    wip: done todo lists

--- crates/request-manager/src/handlers/get_storage_value.rs
+++ crates/request-manager/src/handlers/get_storage_value.rs
@@ -68,6 +68,7 @@ pub async fn get_storage_value(
     );
 
     // 1. request storage
+    tracing::info!("Request storage");
     let response_storage = fact_registry_contract
         .get_storage(
             input.block_number, //
@@ -103,62 +104,149 @@ pub async fn get_storage_value(
         return (StatusCode::OK, Json(&result_string)).into_response();
     } else {
         // 2. request storage proof by calling `call_mev_blocker_api` in proof-generator
-        let api_input = Input {
-            account_address: input.account_proof.address.to_string(),
-            storage_keys: input.storage_keys.clone(),
+        tracing::info!("Request storage proof");
+        let storage_proof = {
+            let api_input = Input {
+                account_address: input.account_proof.address.to_string(),
+                storage_keys: input.storage_keys.clone(),
+            };
+
+            // I think the name `call_mev_blocker_api` should be changed
+            let response = call_mev_blocker_api(State(app_state.client.clone()), Json(api_input))
+                .await
+                .into_response();
+
+            let bytes = match axum::body::to_bytes(response.into_body(), usize::MAX).await {
+                Ok(bytes) => bytes,
+                Err(err) => {
+                    tracing::error!("Error converting response body to bytes: {:?}", err);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Error converting response body to bytes"),
+                    )
+                        .into_response();
+                }
+            };
+
+            let storage_proof: StorageProof = match serde_json::from_slice(&bytes) {
+                Ok(proof) => proof,
+                Err(err) => {
+                    tracing::error!("Error deserializing response to StorageProof: {:?}", err);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Error deserializing response to StorageProof"),
+                    )
+                        .into_response();
+                }
+            };
+
+            storage_proof
         };
-
-        // I think the name `call_mev_blocker_api` should be changed
-        let response = call_mev_blocker_api(State(app_state.client.clone()), Json(api_input))
-            .await
-            .into_response();
-
-        // TODO let storage_proof = func(response) ...
 
         // 3. request state_root by calling `get_state_root` in l1_headers_store
-        let state_root = l1_headers_store_contract
-            .get_state_root(
-                input.block_number, //
-            )
-            .await;
-
-        let state_root = match state_root {
-            Ok(res) => {
-                tracing::info!("Result state_root: {:?}", res.len());
-                res
-            }
-            Err(err) => {
-                tracing::error!("Error state_root: {:?}", err);
-                return (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    Json("Error, get_state_root"),
-                )
-                    .into_response();
-            }
+        tracing::info!("Request state_root by calling `get_state_root` in l1_headers_store");
+        let _state_root = {
+            match l1_headers_store_contract
+                .get_state_root(input.block_number)
+                .await
+            {
+                Ok(res) => {
+                    tracing::info!("Result state_root: {:?}", res.len());
+                    res
+                }
+                Err(err) => {
+                    tracing::error!("Error state_root: {:?}", err);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Error, get_state_root"),
+                    )
+                        .into_response();
+                }
+            };
         };
 
-        // TODO 3-ALT.
+        // 3-ALT.
         //        request the RLP Encoded Block from the proof_generator
         //        then process the block with the starknet_handler, which will save the state_root
+        {
 
-        // TODO 4. request storage proof again
-        let api_input = Input {
-            account_address: input.account_proof.address.to_string(),
-            storage_keys: input.storage_keys,
+            // let rlp_encoded_block = proof_generator.get_rlp_encoded_block();
+            // starknet_handler::interface::process_block(rlp_encoded_block);
+        }
+
+        // 4. request storage proof again
+        tracing::info!("Request storage proof");
+        let storage_proof = {
+            let api_input = Input {
+                account_address: input.account_proof.address.to_string(),
+                storage_keys: input.storage_keys.clone(),
+            };
+
+            // I think the name `call_mev_blocker_api` should be changed
+            let response = call_mev_blocker_api(State(app_state.client.clone()), Json(api_input))
+                .await
+                .into_response();
+
+            let bytes = match axum::body::to_bytes(response.into_body(), usize::MAX).await {
+                Ok(bytes) => bytes,
+                Err(err) => {
+                    tracing::error!("Error converting response body to bytes: {:?}", err);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Error converting response body to bytes"),
+                    )
+                        .into_response();
+                }
+            };
+
+            let storage_proof: StorageProof = match serde_json::from_slice(&bytes) {
+                Ok(proof) => proof,
+                Err(err) => {
+                    tracing::error!("Error deserializing response to StorageProof: {:?}", err);
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Error deserializing response to StorageProof"),
+                    )
+                        .into_response();
+                }
+            };
+            storage_proof
         };
-        let response = call_mev_blocker_api(State(app_state.client), Json(api_input))
-            .await
-            .into_response();
 
-        // TODO let storage_proof = func(response) ...
+        // 5. check if Account Already verified
+        {
+            tracing::info!("Check if account already verified");
+            // let is_verified_account =
+            //     starknet_handler_interface.check_account_proof_status(input.account_proof);
+            // if is_verified_account == false {
+            //     if starknet_handler_interface.verify_account_proof(input.account_proof) == false {
+            //         tracing::error!("Error while verifying the account proof: {:?}", err);
+            //         return (
+            //             StatusCode::INTERNAL_SERVER_ERROR,
+            //             Json("Error while verifying the account proof"),
+            //         )
+            //             .into_response();
+            //     } else {
+            //         //
+            //     }
+            // };
+        }
 
-        // TODO 5. check if Account Already verified
-        // TODO 5-ALT. Account not verified, verify Account proof
-        // TODO 5-ALT2. error RESPONSE, Proof INVALID
+        // 6. verify storage proof
+        {
+            tracing::info!("Verifying the storage proof");
+            // let is_verified_storage =
+            //     starknet_handler_interface.verify_storage_proof(storage_proof);
 
-        // TODO 6. verify storage proof
-        // TODO 6-ALT. proof invalid, error RESPONSE
+            // if is_verified_storage == false {
+            //     tracing::error!("Error while verifying the storage proof: {:?}", err);
+            //     return (
+            //         StatusCode::INTERNAL_SERVER_ERROR,
+            //         Json("Error while verifying the storage proof"),
+            //     );
+            // }
+        }
 
-        return (StatusCode::OK, Json("wip")).into_response();
+        return (StatusCode::OK, Json("done")).into_response();
     }
 }
