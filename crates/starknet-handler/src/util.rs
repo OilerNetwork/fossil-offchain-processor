dfd115d9e12eb24086850ebedbb4baad49925463
c0np4nn4 <c0np4nn4@gmail.com>
2024-07-03 14:18:38 +0900

    docs: add inline rustdocs

--- crates/starknet-handler/src/util.rs
+++ crates/starknet-handler/src/util.rs
@@ -5,6 +5,15 @@ use starknet::core::types::FieldElement;
 
 use crate::error::{FieldElementParseError, HandlerError};
 
+/// Splits a U256 state root into two u128 values representing the high and low parts.
+///
+/// # Arguments
+///
+/// * `state_root` - A string representing the state root.
+///
+/// # Returns
+///
+/// A tuple containing the high (u128) and low (u128) parts of the state root.
 pub fn get_high_and_low(state_root: String) -> (u128, u128) {
     let state_root = U256::from_str(state_root.as_str()).unwrap();
     let state_root_low = state_root.low_u128();
@@ -12,6 +21,16 @@ pub fn get_high_and_low(state_root: String) -> (u128, u128) {
     (state_root_high, state_root_low)
 }
 
+/// Prepares an array of data for use in a StarkNet contract call.
+///
+/// # Arguments
+///
+/// * `data` - A vector of data elements that implement the `ToString` trait.
+///
+/// # Returns
+///
+/// A result containing a tuple with the length of the data (as a `FieldElement`) and the data itself (as a vector of `FieldElement`),
+/// or a `HandlerError` if there is an error during conversion.
 pub fn prepare_array_data<T: ToString>(
     data: Vec<T>,
 ) -> Result<(FieldElement, Vec<FieldElement>), HandlerError> {
