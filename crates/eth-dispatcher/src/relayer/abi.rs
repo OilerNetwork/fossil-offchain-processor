3213b8e1087425f508ff98014269802f87a9b2ff
c0np4nn4 <c0np4nn4@gmail.com>
2024-07-04 17:43:31 +0900

    fix: some lint-rust errors

--- crates/eth-dispatcher/src/relayer/abi.rs
+++ crates/eth-dispatcher/src/relayer/abi.rs
@@ -1,21 +1,19 @@
 //! Module for generating type-safe bindings to the L1MessageSender contract.
 //!
-//! This module uses `abigen` macro to generate Rust bindings for the L1MessageSender contract,
+//! This module uses the `abigen` macro to generate Rust bindings for the L1MessageSender contract,
 //! allowing for type-safe interactions with the contract.
 
 use ethers::middleware::contract::abigen;
 
-/// Generates Rust bindings for the L1MessageSender contract.
-///
-/// The contract includes the following functions:
-///
-/// - `constructor(address snMessaging, uint256 l2RecipientAddr)`
-/// - `function l2RecipientAddr() public view returns (uint256)`
-/// - `function sendExactParentHashToL2(uint256 blockNumber_) external payable`
-/// - `function sendLatestParentHashToL2() external payable`
-///
-/// These bindings enable type-safe interactions with the contract from Rust code.
-
+// The abigen! macro generates Rust bindings for the L1MessageSender contract, including
+// the following functions:
+//
+// - `constructor(address snMessaging, uint256 l2RecipientAddr)`
+// - `function l2RecipientAddr() public view returns (uint256)`
+// - `function sendExactParentHashToL2(uint256 blockNumber_) external payable`
+// - `function sendLatestParentHashToL2() external payable`
+//
+// These bindings enable type-safe interactions with the contract from Rust code.
 abigen!(
     L1MessageSender,
     r#"[
@@ -25,3 +23,4 @@ abigen!(
         function sendLatestParentHashToL2() external payable
     ]"#;
 );
+
