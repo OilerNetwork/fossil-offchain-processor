0e8f36804a38855f0834642ff243e715d70cb86c
Alex Metelli <alex-metelli@gmx.com>
2024-09-03 01:46:36 +0800

    setup bare bone server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
