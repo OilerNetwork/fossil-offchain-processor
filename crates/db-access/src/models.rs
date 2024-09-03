f4aac1edbb1e516f98695778cf90f75981c8c8cf
Alex Metelli <alex-metelli@gmx.com>
2024-09-03 17:54:36 +0800

    added db models

---
+++ crates/db-access/src/models.rs
@@ -0,0 +1,27 @@
+#[derive(sqlx::FromRow, Debug)]
+pub struct BlockHeader {
+    pub block_hash: Option<String>,
+    pub number: i64,
+    pub gas_limit: Option<i64>,
+    pub gas_used: Option<i64>,
+    pub base_fee_per_gas: Option<String>,
+    pub nonce: Option<String>,
+    pub transaction_root: Option<String>,
+    pub receipts_root: Option<String>,
+    pub state_root: Option<String>,
+}
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct Transaction {
+    pub block_number: Option<i64>, // Make this Option<i64> to handle possible NULLs
+    pub transaction_hash: String,
+    pub transaction_index: Option<i32>, // Make this Option<i32> to handle possible NULLs
+    pub from_addr: Option<String>,      // Make these fields Option<String> to handle possible NULLs
+    pub to_addr: Option<String>,
+    pub value: Option<String>,
+    pub gas_price: Option<String>,
+    pub max_priority_fee_per_gas: Option<String>,
+    pub max_fee_per_gas: Option<String>,
+    pub gas: Option<String>,
+    pub chain_id: Option<String>,
+}
