aa7b17793d3bc5ddc8a9cb31730f5b736c6e2739
Alex Metelli <alex-metelli@gmx.com>
2024-09-03 17:54:25 +0800

    added db connection

---
+++ crates/db-access/src/lib.rs
@@ -0,0 +1,25 @@
+mod models;
+mod queries;
+
+use dotenv::dotenv;
+use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+pub struct DbConnection {
+    pub pool: PgPool,
+}
+
+impl DbConnection {
+    pub async fn new() -> Result<Self, sqlx::Error> {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&database_url)
+            .await?;
+
+        Ok(Self { pool })
+    }
+}
