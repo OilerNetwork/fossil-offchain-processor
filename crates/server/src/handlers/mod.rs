ea48b655594defceba963a747b1fcb551a15fefa
chen <23054115+cwkang1998@users.noreply.github.com>
2024-09-18 12:52:21 +0800

    feat: add basic api server boilerplate

--- crates/server/src/handlers/mod.rs
+++ crates/server/src/handlers/mod.rs
@@ -1,1 +1,46 @@
+use axum::{http::StatusCode, Json};
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+pub struct JobRequest {
+    identifier: Vec<String>,
+    timestamp: u128,
+    params: Vec<String>,
+}
+
+// We can keep this as a simple "Hello, world!" for now
+// but its a good place to place a health check endpoint
+pub async fn root() -> &'static str {
+    "Hello, world!"
+}
+
+pub async fn get_twap(Json(payload): Json<JobRequest>) -> (StatusCode, &'static str) {
+    (StatusCode::OK, "twap")
+}
+
+pub async fn get_volatility(Json(payload): Json<JobRequest>) -> (StatusCode, &'static str) {
+    (StatusCode::OK, "volatility")
+}
+
+pub async fn get_reserve_price(Json(payload): Json<JobRequest>) -> (StatusCode, &'static str) {
+    (StatusCode::OK, "reserve_price")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{http::StatusCode, routing::get, Router};
+
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn test_root() {
+        let app = Router::new().route("/", get(root));
+        let server = TestServer::new(app).unwrap();
+
+        let response = server.get("/").await;
+
+        assert_eq!(response.status_code(), StatusCode::OK);
+        assert_eq!(response.text(), "Hello, world!");
+    }
+}
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,8 +1,13 @@
 use axum::{routing::get, Router};
+use server::handlers;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Welcome to Fossil!" }));
+    let app = Router::new()
+        .route("/", get(handlers::root))
+        .route("/twap", get(handlers::get_twap))
+        .route("/volatility", get(handlers::get_volatility))
+        .route("/reserve_price", get(handlers::get_reserve_price));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
