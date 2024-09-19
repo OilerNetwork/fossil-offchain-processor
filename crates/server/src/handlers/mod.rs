662458fa0883514b86897e86e2ad49622bb4627b
Chen Wen Kang <23054115+cwkang1998@users.noreply.github.com>
2024-09-19 16:15:52 +0800

    feat: add basic api server boilerplate (#17)
    * feat: add basic api server boilerplate
    
    * feat: combine diff job requests into a single api
    
    * feat: add response & callback struct

--- crates/server/src/handlers/mod.rs
+++ crates/server/src/handlers/mod.rs
@@ -1,1 +1,61 @@
+use axum::{http::StatusCode, Json};
+use serde::{Deserialize, Serialize};
 
+// timestamp ranges for each sub-job calculation
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PitchLakeJobRequestParams {
+    twap: (u64, u64),
+    volatility: (u64, u64),
+    reserve_price: (u64, u64),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PitchLakeJobRequest {
+    identifiers: Vec<String>,
+    params: PitchLakeJobRequestParams,
+}
+
+// TODO: Placeholder for now, need to be more generic
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PitchLakeJobCallback {
+    job_id: String,
+    twap: u64,
+    volatility: u64,
+    reserve_price: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct JobResponse {
+    job_id: String,
+}
+
+// We can keep this as a simple "Hello, world!" for now
+// but its a good place to place a health check endpoint
+pub async fn root() -> &'static str {
+    "Hello, world!"
+}
+
+pub async fn get_pricing_data(
+    Json(payload): Json<PitchLakeJobRequest>,
+) -> (StatusCode, &'static str) {
+    (StatusCode::OK, "pricing_data")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{http::StatusCode, routing::get, Router};
+
+    use axum_test::TestServer;
+
+    #[tokio::test]
+    async fn test_root() {
+        let app = Router::new().route("/", get(root));
+        let server = TestServer::new(app).unwrap();
+
+        let response = server.get("/").await;
+
+        assert_eq!(response.status_code(), StatusCode::OK);
+        assert_eq!(response.text(), "Hello, world!");
+    }
+}
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,8 +1,11 @@
 use axum::{routing::get, Router};
+use server::handlers;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Welcome to Fossil!" }));
+    let app = Router::new()
+        .route("/", get(handlers::root))
+        .route("/pricing_data", get(handlers::get_pricing_data));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
