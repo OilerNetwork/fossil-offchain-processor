0da9b3f24f570b7c790351fe28853ba474936afe
Alex Metelli <alex-metelli@gmx.com>
2024-10-09 05:40:48 +0800

    WIP added logic for block validity and valid chain verification

--- crates/db-access/src/main.rs
+++ crates/db-access/src/main.rs
@@ -1,18 +1,25 @@
-use db_access::models::BlockHeaderSubset;
-use db_access::queries::get_base_fees_between_blocks;
-use db_access::DbConnection;
+use db_access::rpc::get_block_by_number;
+use eth_rlp_verify::verify_block;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // Connect to DB
-    let db = DbConnection::new().await?;
+async fn main() -> Result<(), Box<dyn Error>> {
+    let block_number: u64 = 13733852;
+    let block = get_block_by_number(block_number).await?;
+    let block_hash = block.block_hash.clone();
+    let parent_hash = block.parent_hash.clone();
 
-    // Get block headers
-    let start_block = 13733852;
-    let end_block = start_block + 5;
-    let block_headers: Vec<BlockHeaderSubset> =
-        get_base_fees_between_blocks(&db.pool, start_block, end_block).await?;
+    let is_valid = verify_block(block_number, block, &block_hash);
 
-    println!("{:?}", block_headers);
+    let parent_block = get_block_by_number(block_number - 1).await?;
+    let parent_block_hash = parent_block.block_hash.clone();
+
+    let is_valid_parent = parent_hash.unwrap() == parent_block_hash;
+
+    println!(
+        "The block is valid: {}, and the parent block is valid: {}",
+        is_valid, is_valid_parent
+    );
+
     Ok(())
 }
