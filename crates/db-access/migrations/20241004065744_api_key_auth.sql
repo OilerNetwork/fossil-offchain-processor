f6885a7263d968fdef3e591d37c0349f02669261
Alex Metelli <alex-metelli@gmx.com>
2024-10-21 03:58:38 +0800

    updated migration script for auth keys

--- crates/db-access/migrations/20241004065744_api_key_auth.sql
+++ crates/db-access/migrations/20241004065744_api_key_auth.sql
@@ -1,9 +1,10 @@
--- Add up migration script here
-CREATE TABLE api_keys (
+-- Up migration
+CREATE TABLE IF NOT EXISTS api_keys (
     id SERIAL PRIMARY KEY,
     key TEXT NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
--- Add down migration script here
-DROP TABLE api_keys;
+
+-- Down migration
+DROP TABLE IF EXISTS api_keys;
