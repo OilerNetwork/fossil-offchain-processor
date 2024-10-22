feb70ce43cf1bff6f16633c7666d45fe035830f3
Alex Metelli <alex-metelli@gmx.com>
2024-10-22 14:28:22 +0800

    unit tests for endpoints + fixes

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,51 +1,20 @@
-use axum::{
-    middleware::from_fn_with_state,
-    routing::{get, post},
-    Router,
-};
-use db_access::DbConnection;
 use eyre::Result;
-use server::{handlers, middlewares::auth::simple_apikey_auth};
-use tower_http::{cors::CorsLayer, trace::TraceLayer};
-use tracing::Level;
+use server::create_app;
+use sqlx::PgPool;
 use tracing_subscriber::{filter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let db = DbConnection::new().await?;
+    let pool = PgPool::connect(&std::env::var("DATABASE_URL").unwrap()).await?;
+    let app = create_app(pool).await;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(filter::Targets::new().with_default(Level::DEBUG))
+        .with(filter::Targets::new().with_default(tracing::Level::DEBUG))
         .init();
 
-    // Define routes with specific middleware.
-    let secured_routes = Router::new()
-        .route(
-            "/job_status/:job_id",
-            get(handlers::job_status::get_job_status)
-                .layer(from_fn_with_state(db.clone(), simple_apikey_auth)),
-        )
-        .route(
-            "/pricing_data",
-            post(handlers::get_pricing_data::get_pricing_data)
-                .layer(from_fn_with_state(db.clone(), simple_apikey_auth)),
-        );
-
-    let public_routes = Router::new().route("/health", get(handlers::health_check::health_check));
-
-    // Build the complete application router with middleware layers.
-    let app = Router::new()
-        .merge(secured_routes)
-        .merge(public_routes)
-        .layer(TraceLayer::new_for_http())
-        .layer(CorsLayer::permissive())
-        .with_state(db);
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     tracing::debug!("Listening on {}", listener.local_addr()?);
-
     axum::serve(listener, app).await.unwrap();
-
     Ok(())
 }
