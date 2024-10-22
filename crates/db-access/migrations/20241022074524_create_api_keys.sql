7afb4363ea168c204e058c110b809e240c4271d6
Alex Metelli <alex-metelli@gmx.com>
2024-10-22 15:51:09 +0800

    refacto sqlx migrations

---
+++ crates/db-access/migrations/20241022074524_create_api_keys.sql
@@ -0,0 +1,13 @@
+-- Up: Create api_keys table
+CREATE TABLE IF NOT EXISTS public.api_keys (
+    id integer NOT NULL DEFAULT nextval('api_keys_id_seq'::regclass),
+    key text NOT NULL,
+    name character varying(255) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    CONSTRAINT api_keys_pkey PRIMARY KEY (id),
+    CONSTRAINT api_keys_key_key UNIQUE (key)
+);
+ALTER TABLE IF EXISTS public.api_keys OWNER TO postgres;
+
+-- Down: Drop api_keys table
+DROP TABLE IF EXISTS public.api_keys;
