c87a4a86b380feb9a558c99d09056d8dc318097f
Alex Metelli <alex-metelli@gmx.com>
2024-10-24 12:47:39 +0800

    removed headers related migrations

--- crates/db-access/migrations/20241024035948_blockheaders.down.sql
+++
@@ -1,8 +0,0 @@
--- Drop the indexes if they exist
-DROP INDEX IF EXISTS public.idx_blockheaders_base_fee;
-DROP INDEX IF EXISTS public.idx_blockheaders_number;
-DROP INDEX IF EXISTS public.idx_blockheaders_number_base_fee;
-DROP INDEX IF EXISTS public.idx_blockheaders_timestamp;
-
--- Drop the blockheaders table if it exists
-DROP TABLE IF EXISTS public.blockheaders;
--- crates/db-access/migrations/20241024040004_transactions.down.sql
+++
@@ -1,5 +0,0 @@
--- Drop the transactions table if it exists
-DROP TABLE IF EXISTS public.transactions;
-
--- Drop the index if it exists
-DROP INDEX IF EXISTS public.transactions_blocknumber_idx;
