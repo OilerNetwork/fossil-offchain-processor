6a88f0c64c7f3d2d75ec5db6e7ed1c4e4900b8c8
Alex Metelli <alex-metelli@gmx.com>
2024-11-19 04:33:26 +0800

    added api key script target

--- crates/server/src/scripts/create_api_key.rs
+++ crates/server/src/scripts/create_api_key.rs
@@ -1,12 +1,17 @@
-use std::env;
-
 use db_access::{auth::add_api_key, DbConnection};
 use eyre::Result;
+use std::env;
 use tracing::info;
+use tracing_subscriber;
 use uuid::Uuid;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Set up the tracing subscriber with INFO level
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let db = DbConnection::new().await?;
 
     let name = env::args()
@@ -18,7 +23,7 @@ async fn main() -> Result<()> {
 
     add_api_key(&db.pool, api_key.clone(), name).await?;
 
-    info!("API Key: {}", api_key);
+    info!("API Key Created: {}", api_key);
 
     Ok(())
 }
