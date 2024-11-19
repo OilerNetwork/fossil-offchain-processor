e065cb68149148b721b8de9028c0150168e26501
Alex Metelli <alex-metelli@gmx.com>
2024-11-19 14:57:33 +0800

    removed mmr-accumulator crate

--- crates/mmr-accumulator/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![deny(unused_crate_dependencies)]
-use tracing_subscriber as _;
-
-pub mod error;
-pub mod ethereum;
-pub mod processor_utils;
-pub mod store;
-pub use block_validity::BlockHeader;
-pub use mmr::MMR;
