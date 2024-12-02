f3fc97825b010fac71fd02ad72826a87d321e4df
Alex Metelli <alex-metelli@gmx.com>
2024-12-02 15:54:38 +0800

    improved logging

--- crates/server/src/middlewares/auth.rs
+++ crates/server/src/middlewares/auth.rs
@@ -23,22 +23,28 @@ pub async fn simple_apikey_auth(
     // Extract the API key from headers.
     if let Some(incoming_api_key) = headers.get("x-api-key") {
         if let Ok(api_key_str) = incoming_api_key.to_str() {
+            tracing::info!("Attempting authentication with API key");
             tracing::debug!("Received API key: {}", api_key_str);
             // Access the database connection from the state.
             let matching_api_key = find_api_key(&state.db.pool, api_key_str.to_string()).await;
 
             return match matching_api_key {
                 Ok(_) => {
+                    tracing::info!("Authentication successful");
                     tracing::debug!("API key authenticated successfully");
                     Ok(next.run(request).await)
                 }
                 Err(err) => {
+                    tracing::warn!("Authentication failed: Invalid API key");
                     tracing::debug!("Authentication failed: {:?}", err);
                     Ok((StatusCode::UNAUTHORIZED, Json(response_data)).into_response())
                 }
             };
+        } else {
+            tracing::warn!("Authentication failed: Invalid API key format");
         }
     } else {
+        tracing::warn!("Authentication failed: No API key provided");
         tracing::debug!("No API key found in headers");
     }
 
