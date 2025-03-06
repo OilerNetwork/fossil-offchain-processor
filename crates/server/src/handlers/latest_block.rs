4f371a8c1d633894175ada8deef9dd1fb492601c
Dhruv Gupta <dhruv035@gmail.com>
2025-03-06 18:21:20 +0530

    Update test

--- crates/server/src/handlers/latest_block.rs
+++ crates/server/src/handlers/latest_block.rs
@@ -86,8 +86,8 @@ mod tests {
         ctx.create_block(12345, "1234567890".to_string(), 0).await;
         ctx.create_block(12346, "1234567891".to_string(), 0).await;
         let latest_block = 12347;
-        let latest_timestamp = "1234567892";
-        ctx.create_block(latest_block, latest_timestamp.to_string(), 0)
+        let latest_timestamp = "1234567892".to_string();
+        ctx.create_block(latest_block, latest_timestamp.clone(), 0)
             .await;
 
         let (status, Json(response)) = ctx.get_latest_block().await;
