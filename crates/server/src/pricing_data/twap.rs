ca2cfc276a366463c8d9660bff073093b34a0858
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-04-01 12:18:21 -0400

    add pricing util functions
    - prepare data frame
    - shared: `replace_timestamp_with_date` & `group_by_1h_interfaces`

--- crates/server/src/pricing_data/twap.rs
+++ crates/server/src/pricing_data/twap.rs
@@ -1,34 +1,10 @@
-use super::utils::hex_string_to_f64;
+use super::utils::prepare_data_frame;
 use db_access::models::BlockHeader;
-use eyre::{anyhow as err, eyre, Result};
+use eyre::{eyre, Result};
 use polars::prelude::*;
 
 pub async fn calculate_twap(block_headers: Vec<BlockHeader>) -> Result<f64> {
-    if block_headers.is_empty() {
-        return Err(err!("No block headers provided."));
-    }
-
-    // Prepare DataFrame
-    let mut timestamps = Vec::new();
-    let mut base_fees = Vec::new();
-
-    for header in block_headers {
-        let timestamp = header
-            .timestamp
-            .ok_or_else(|| err!("No timestamp in header"))?;
-        let base_fee = hex_string_to_f64(
-            &header
-                .base_fee_per_gas
-                .ok_or_else(|| err!("No base fee in header"))?,
-        )?;
-        timestamps.push(timestamp);
-        base_fees.push(base_fee);
-    }
-
-    let df = DataFrame::new(vec![
-        Series::new("timestamp".into(), timestamps.clone()),
-        Series::new("base_fee".into(), base_fees),
-    ])?;
+    let df = prepare_data_frame(block_headers)?;
 
     let mean = df
         .column("base_fee")?
